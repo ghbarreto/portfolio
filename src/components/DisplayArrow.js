afd192b89f53bfcfd1a28f4d7662fbc3296bce77
ivlen <gh.barreto@hotmail.com>
2021-09-21 23:25:14 -0300

    commit for tonight

---
+++ src/components/DisplayArrow.js
@@ -0,0 +1,28 @@
+import './scss/DisplayArrow.scss';
+import { connect } from 'react-redux';
+
+const DisplayArrow = ({ details }) => {
+  const changeUlPosition = details.details
+    ? details.details.location !== 'Reach'
+      ? 'main-style bounce'
+      : 'main-styleUp bounceUp'
+    : null;
+  const changeArrowPosition = details.details
+    ? details.details.location !== 'Reach'
+      ? 'arrow-down'
+      : 'arrow-up'
+    : null;
+  return (
+    <ul className={changeUlPosition}>
+      <li className={`${changeArrowPosition} arrow`}></li>
+    </ul>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    details: state.details,
+  };
+};
+
+export default connect(mapStateToProps)(DisplayArrow);
