ccd57946e7df78d47581a2a6e72d31bd40b0f562
Gabriel <gabriel@kyte.com.br>
2021-10-05 15:30:02 -0300

    vertical project list

--- client/src/components/screens/sub/MobileWorkTimeline.js
+++ client/src/components/screens/sub/MobileWorkTimeline.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import { logo, timelineHorizontal, tags } from '../../utils/utils.functions';
+import _ from 'lodash';
 import '../../scss/MobileWorkTimeline.scss';
 
 const MobileWorkTimeline = () => {
   const createWorkTimeline = () => {
-    return (
-      <>
-        <div className="mobile-container">
-          <div className="mobile-dot"></div>
-          <div className="mobile-border"></div>
-          <div className="mobile-arrow"></div>
-          <div className="mobile-title">TITLE </div>
-          <div className="mobile-image">IMAGE </div>
-          <div className="mobile-paragraph">PARAGRAPH</div>
-        </div>
-      </>
-    );
+    return _.map(timelineHorizontal, (e, index) => {
+      index++;
+      const arrows = index % 2 === 0 ? 'arrow-left' : 'arrow-right';
+      return (
+        <>
+          <div className="mobile-container">
+            <div className={`mobile-dot mobile-dot-${index}`}></div>
+            <div
+              className={`mobile-border mobile-border-${index} mobile-border-left-${index}`}
+            ></div>
+            <div
+              className={`mobile-arrow mobile-arrow-${index} ${arrows} arrow-left-${index}`}
+            ></div>
+            <div className={`mobile-image mobile-image-left-${index}`}>
+              <div className="mobile-image-pos">
+                <img src={e.image} alt="a" />
+              </div>
+            </div>
+            <div className={`mobile-title mobile-title-${index}`}>
+              <span className={`color-${index}`}>{e.title} </span>
+              <span className="back-slashes">
+                {tags.slash}
+                {tags.slash}
+              </span>{' '}
+              <span className={`color-${index}`}>{e.date}</span>
+            </div>
+            <div className={`mobile-paragraph mobile-paragraph-${index}`}>
+              {e.description}
+            </div>
+          </div>
+        </>
+      );
+    });
   };
   return (
     <div className="vertical-project-list">
       <div className="vertical-line">{createWorkTimeline()}</div>
+      <div className="vertical-line-arrow"></div>
     </div>
   );
 };
